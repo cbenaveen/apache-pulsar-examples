813de91863cee0bf24ce114185347d06c59793cf
Naveen Kumar <cbenaveen@gmail.com>
2022-02-18 09:59:05 +0530

    InitCap function impl

--- src/main/java/org/apachepulsar/example/functions/InitCapFunction.java
+++ src/main/java/org/apachepulsar/example/functions/InitCapFunction.java
@@ -1,4 +1,11 @@
 package org.apachepulsar.example.functions;
 
-public class InitCapFunction {
+import java.util.function.Function;
+
+public class InitCapFunction implements Function<String, String> {
+    @Override
+    public String apply(String s) {
+        String s1 = Character.toUpperCase(s.charAt(0)) + s.substring(1);
+        return s1;
+    }
 }
