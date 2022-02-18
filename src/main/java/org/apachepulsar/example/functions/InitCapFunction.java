9e91c4f0cbb2c41e4abef8110aeae3f9eb938907
Naveen Kumar <cbenaveen@gmail.com>
2022-02-18 10:18:06 +0530

    Added a message validation and throwing exception

--- src/main/java/org/apachepulsar/example/functions/InitCapFunction.java
+++ src/main/java/org/apachepulsar/example/functions/InitCapFunction.java
@@ -1,11 +1,14 @@
 package org.apachepulsar.example.functions;
 
+import java.util.Objects;
 import java.util.function.Function;
 
 public class InitCapFunction implements Function<String, String> {
     @Override
     public String apply(String s) {
-        String s1 = Character.toUpperCase(s.charAt(0)) + s.substring(1);
-        return s1;
+        if (Objects.nonNull(s) && s.length() > 0) {
+            return Character.toUpperCase(s.charAt(0)) + s.substring(1);
+        }
+        throw new IllegalArgumentException("Incoming message length is invalid");
     }
 }
