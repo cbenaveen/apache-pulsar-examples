1f3d456d915e0fc90261458954076e1638106337
Naveen Kumar <cbenaveen@gmail.com>
2022-02-24 09:54:16 +0530

    Externalized the broker url

--- src/main/java/org/apachepulsar/example/producers/SimpleMessageProducer.java
+++ src/main/java/org/apachepulsar/example/producers/SimpleMessageProducer.java
@@ -3,6 +3,7 @@ package org.apachepulsar.example.producers;
 import org.apache.pulsar.client.api.*;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -10,11 +11,19 @@ import java.util.Objects;
  * to give topic.
  */
 public class SimpleMessageProducer {
-    public static final String PULSAR_SERVICE_URL = "pulsar://localhost:6650";
+    /**
+     * In developer machine you would connect to pulsar://localhost:6650
+     * in QA environment you would connect to pulsar://qa.pulsar.proxy:6650
+     * in Stage environment you would connect to pulsar://stage.pulsar.proxy:6650
+     * in Prod environment you would connect to pulsar://prod.pulsar.proxy:7650
+     */
+//    public static final String PULSAR_SERVICE_URL = "pulsar://localhost:6650";
     public static final String TOPIC_NAME = "persistent://research-and-development/research/greeting";
 
     public static PulsarClient getPulsarClient() throws PulsarClientException {
-        return PulsarClient.builder().serviceUrl(PULSAR_SERVICE_URL).build();
+        Map<String, String> getenv = System.getenv();
+        final String pulsarServiceHost = getenv.get("PULSAR_SERVICE_URL");
+        return PulsarClient.builder().serviceUrl(pulsarServiceHost).build();
     }
 
     public static void main(String[] args) throws PulsarClientException {
